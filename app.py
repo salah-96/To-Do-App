82700b3541b741552d27d0d7426c19348837a4d5
Salah Abukar <146073941+salah-96@users.noreply.github.com>
2023-11-01 10:46:39 +0100

    added templates folder and created a html file inside the folder

--- app.py
+++ app.py
@@ -1,6 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
+tasks = [
+    "Buy groceries",
+    "Cleaning",
+    "Take out the trash",
+]
+
 
 @app.route('/')
-def hello_world():
-    return 'hello_world'
+def home():
+    return render_template('home.html', tasks=tasks)
+
+
+@app.route('/add', methods=['POST'])
+def add_task():
+    task = request.form.get('task')
+    tasks.append(task)
+    return render_template('home.html', tasks=tasks)
+
+
+@app.route('/delete', methods=['POST'])
+def delete_task():
+    task = request.form.get('task')
+    if task in tasks:
+        tasks.remove(task)
+    return render_template('home.html', tasks=tasks)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
